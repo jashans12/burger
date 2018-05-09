aadde807df884b6ec7c270f37c1d68bc28ae7782
jashans12 <hansenjason12@gmail.com>
2018-05-09 09:59:20 -0600

    working app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Deluxe');
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
---
+++ models/burger.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  create: function(name, cb) {
+    orm.create("burgers", name, cb);
+  },
+
+  update: function(id, cb) {
+    orm.update("burgers", id, cb);
+  }
+
+}
+
+module.exports = burger;
